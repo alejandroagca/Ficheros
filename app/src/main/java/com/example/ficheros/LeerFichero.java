f574e23548eec8b645e813696aacb74b51d4ff97
alejandroagca <alejandroagca1995@gmail.com>
2017-10-17 12:44:52 +0200

    Primer commit. Ejercicio 1 finalizado

---
+++ app/src/main/java/com/example/ficheros/LeerFichero.java
@@ -0,0 +1,13 @@
+package com.example.ficheros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeerFichero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leer_fichero);
+    }
+}
